59cb5b452033bde9529c3a2e19c055d0e8318b7c
sroy4 <souvikroy07@gmail.com>
2016-12-04 17:42:59 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -123,6 +123,11 @@
 });
 
 var trained=0;
+var model;
+//var features=["Age", "Sex", "On_thyroxine", "Query_on_thyroxine", "On_antithyroid_medication", "Sick", "Pregnant", "Thyroid Surgery", "I131_treatment", "Query_hypothyroid", "Query_hyperthyroid", "Lithium", "Goiter", "Tumor", "Hypopituitary", "Pysch", "TSH", "T3", "TT4", "T4U", "FTI"];
+var features=["Age", "Sex", "On_thyroxine", "On_antithyroid_medication", "Sick", "Pregnant", "Thyroid Surgery", "I131_treatment", "Lithium", "Goiter", "Tumor", "Hypopituitary", "Pysch", "TSH", "T3", "TT4", "T4U", "FTI"];
+var overlayText = document.getElementById("overlayText");
+
 function getRadioCheckedValue(radio_name)
 {
    var oRadio = document.thyroid_form.elements[radio_name];
@@ -171,34 +176,18 @@ function Perceptron(input, hidden, output)
 Perceptron.prototype = new synaptic.Network();
 Perceptron.prototype.constructor = Perceptron;
 
-function myFunction() {
-
-
-
-features=["Age", "Sex", "On_thyroxine", "Query_on_thyroxine", "On_antithyroid_medication", "Sick", "Pregnant", "Thyroid Surgery", "I131_treatment", "Query_hypothyroid", "Query_hyperthyroid", "Lithium", "Goiter", "Tumor", "Hypopituitary", "Pysch", "TSH", "T3", "TT4", "T4U", "FTI"];
-
-// prediction_input=[];
-// prediction_input.push(document.thyroid_form.age.valueAsNumber/100);
-// for (var j=1;j<16;j++){
-//     prediction_input.push(parseInt(getRadioCheckedValue(features[j])));
-
-// }
-// prediction_input.push(document.thyroid_form.TSH.valueAsNumber/1000);
-// prediction_input.push(document.thyroid_form.T3.valueAsNumber/10000);
-// prediction_input.push(document.thyroid_form.TT4.valueAsNumber/100);
-// prediction_input.push(document.thyroid_form.T4U.valueAsNumber/10);
-// prediction_input.push(document.thyroid_form.FTI.valueAsNumber/100);
-
-// console.log(prediction_input);
-
-if (trained==0){
+function train() {
+toggle_visibility("overlay");
+document.getElementById("overlayText").innerText="Training.....\n"
 
 trainingSet=[];
 testSet=[];
 var Architect = synaptic.Architect;
 var Trainer = synaptic.Trainer;
+
+
 //var myPerceptron = new Architect.Perceptron(21,10,1);    
-var myPerceptron = new Perceptron(21,3,1);
+var myPerceptron = new Perceptron(features.length,3,1);
 d3.csv("/data/UCI Thyroid Train_Anomaly.csv", function(data) {
     data.forEach(function(d) {
     var input_array=[];
@@ -232,13 +221,24 @@ d3.csv("/data/UCI Thyroid Test_Anomaly.csv", function(data) {
 
 
 var trainer = new Trainer(myPerceptron);
+
 trainer.train(trainingSet,{
     rate: .01,
-    iterations: 20000,
+    iterations: 1000,
     error: .005,
     shuffle: true,
     log: 1000,
-    cost: Trainer.cost.CROSS_ENTROPY
+    cost: Trainer.cost.CROSS_ENTROPY,
+    schedule: {
+        every: 1000, // repeat this task every 500 iterations
+        do: function(data) {
+            // custom log
+            document.getElementById("overlayText").innerText=document.getElementById("overlayText").innerText+"Error: "+ (data.error).toFixed(4)+ "     "+ "Iterations: "+ data.iterations + "\n" ;
+            //console.log("error", data.error, "iterations", data.iterations, "rate", data.rate);
+            //if (someCondition)
+             //   return true; // abort/stop training
+        }
+    }
 });
 
 var delay=10000; //10 second
@@ -246,6 +246,7 @@ var delay=10000; //10 second
 setTimeout(function() {
   //your code to be executed after 1 second
   console.log("Wait Over")
+  toggle_visibility("overlay");
   console.log(myPerceptron.activate(testSet[0]['input']));
   var j=0;
   for (var i=0;i<testSet.length;i++){
@@ -259,25 +260,75 @@ setTimeout(function() {
         j=j+1;
     }
 }
-trained=1;
+
 console.log(j/testSet.length);
-//console.log(myPerceptron.activate(prediction_input));
 console.log(myPerceptron.neurons());
+var exported = myPerceptron.toJSON();
+model = myPerceptron;
+trained=1;
+//console.log(exported);
+//download(JSON.stringify(exported), 'model2.txt', 'text/plain');
+
+
 }, delay);
 
-
-    });    
-
-
-
+});
 
 });
 
 }
-else{
 
-    //console.log(myPerceptron.activate(prediction_input));
-}
+function predict(){
+
+
+    var Network = synaptic.Network;
+
+    prediction_input=[];
+    prediction_input.push(document.thyroid_form.age.valueAsNumber/100);
+    for (var j=1;j<features.length-5;j++){
+        prediction_input.push(parseInt(getRadioCheckedValue(features[j])));
+
+    }
+    prediction_input.push(document.thyroid_form.TSH.valueAsNumber/1000);
+    prediction_input.push(document.thyroid_form.T3.valueAsNumber/10000);
+    prediction_input.push(document.thyroid_form.TT4.valueAsNumber/100);
+    prediction_input.push(document.thyroid_form.T4U.valueAsNumber/10);
+    prediction_input.push(document.thyroid_form.FTI.valueAsNumber/100);
+
+    console.log(prediction_input);
+
+    if (trained==0){
+        var imported_model;
+        jQuery.get('model2.txt', function(data) {
+        
+        imported_model= JSON.parse(data);
+        imported_model = Network.fromJSON(imported_model);
+        console.log(imported_model.neurons());
+        if (imported_model.activate(prediction_input)<0.5){
+            Out=0;
+            alert("You likely don't have hypothyroidism. Please see a medical professional to confirm.");
+        }
+        else{
+            Out=1;
+            
+            alert("You likely have hypothyroidism. Please see a medical professional to confirm. ");
+        }      
+
+        });
+    }
+    else{
+        console.log("Here!");
+        if (model.activate(prediction_input)<0.5){
+            Out=0;
+            alert("You likely don't have hypothyroidism. Please see a medical professional to confirm.");
+            }
+        else{
+            Out=1;
+            
+            alert("You likely have hypothyroidism. Please see a medical professional to confirm. ");
+        }      
+
+    }
 
 }
 function generateRandomBoolean(){
@@ -292,13 +343,8 @@ function generateRandomBoolean(){
 
 function fillBoolean(radio_name){
    var oRadio = document.thyroid_form.elements[radio_name];
- 
-   for(var i = 0; i < oRadio.length; i++)
-   {
-       oRadio[i].checked=generateRandomBoolean();
-      
-   }
-
+   var x= generateRandomBoolean();  
+   oRadio[x].checked=true;
 
 }
 
@@ -308,12 +354,34 @@ function generateRandom(){
     document.thyroid_form.TSH.value=(Math.random()*50).toFixed(2);
     document.thyroid_form.T3.value=(Math.random()*400).toFixed(2);
     document.thyroid_form.TT4.value=(Math.random()*19).toFixed(2);
-    document.thyroid_form.T4U.value=(Math.random()*20).toFixed(2);
+    document.thyroid_form.T4U.value=(Math.random()*1).toFixed(2);
     document.thyroid_form.FTI.value=(Math.random()*29).toFixed(2);
-    features=["Age", "Sex", "On_thyroxine", "Query_on_thyroxine", "On_antithyroid_medication", "Sick", "Pregnant", "Thyroid Surgery", "I131_treatment", "Query_hypothyroid", "Query_hyperthyroid", "Lithium", "Goiter", "Tumor", "Hypopituitary", "Pysch", "TSH", "T3", "TT4", "T4U", "FTI"];
-    for (var j=1;j<16;j++){
+    for (var j=1;j<features.length-5;j++){
          fillBoolean(features[j]);
 
     }
 
 }
+
+function download(text, name, type) {
+    var a = document.createElement("a");
+    var file = new Blob([text], {type: type});
+    a.href = URL.createObjectURL(file);
+    a.download = name;
+    a.click();
+}
+
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+}
+
+function toggle(){
+
+    toggle_visibility("overlay");
+    //document.getElementById("overlayText").innerText="Hshfkdhkdhkgdjkjhk";
+}
+
